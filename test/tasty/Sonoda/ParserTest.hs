a57500c9bb6c6424c7572187ec2fcd6814b358ff
aiya000 <aiya000.develop@gmail.com>
2018-03-21 21:18:31 +0900

    :recycle: Refactor tests

--- test/tasty/Sonoda/ParserTest.hs
+++ test/tasty/Sonoda/ParserTest.hs
@@ -24,9 +24,9 @@ import qualified Data.Text as T
 (Right x) ^== y = x == y
 
 -- | Similar to '^==' but it is in hspec
-isParsedTo :: (HasCallStack, Show e, Show a, Eq a) => Either e a -> a -> Expectation
-isParsedTo (Left  e) _ = expectationFailure $ "a parse is failed: " <> show e
-isParsedTo (Right x) y = x `shouldBe` y
+shouldBeParsedTo :: (HasCallStack, Show e, Show a, Eq a) => Either e a -> a -> Expectation
+shouldBeParsedTo (Left  e) _ = expectationFailure $ "a parse is failed: " <> show e
+shouldBeParsedTo (Right x) y = x `shouldBe` y
 
 
 scprop_natVal_can_be_parsed_correctly :: NonNegative Int -> Bool
@@ -37,10 +37,10 @@ scprop_natVal_can_be_parsed_correctly (NonNegative n) =
 spec_boolVal_unitVal_can_be_parsed_correctly :: Spec
 spec_boolVal_unitVal_can_be_parsed_correctly = do
   it "True,False" $ do
-    parseExpr "True"  `isParsedTo` ExprAtomic (TermBool True)
-    parseExpr "False" `isParsedTo` ExprAtomic (TermBool False)
+    parseExpr "True"  `shouldBeParsedTo` ExprAtomic (TermBool True)
+    parseExpr "False" `shouldBeParsedTo` ExprAtomic (TermBool False)
   it "Unit" $
-    parseExpr "Unit" `isParsedTo` ExprAtomic TermUnit
+    parseExpr "Unit" `shouldBeParsedTo` ExprAtomic TermUnit
 
 
 prop_identifiers_can_be_parsed_correctly :: CamelName -> Bool
@@ -51,43 +51,49 @@ prop_identifiers_can_be_parsed_correctly (unCamelName &&& (unCamelName >>> T.unp
 spec_types_can_be_parsed_correctly :: Spec
 spec_types_can_be_parsed_correctly = do
   describe "atomic types" $ do
-    it "Nat"  $ parseType "Nat"  `isParsedTo` natT
-    it "Bool" $ parseType "Bool" `isParsedTo` boolT
-    it "Unit" $ parseType "Unit" `isParsedTo` unitT
+    it "Nat"  $ parseType "Nat"  `shouldBeParsedTo` natT
+    it "Bool" $ parseType "Bool" `shouldBeParsedTo` boolT
+    it "Unit" $ parseType "Unit" `shouldBeParsedTo` unitT
   describe "arrow types" $ do
     it "Nat -> Nat" $
-      parseType "Nat -> Nat" `isParsedTo` (natT ~> natT)
+      parseType "Nat -> Nat" `shouldBeParsedTo` (natT ~> natT)
     it "Nat -> Bool -> Unit" $
-      parseType "Nat -> Bool -> Unit" `isParsedTo` (natT ~> boolT ~> unitT)
+      parseType "Nat -> Bool -> Unit" `shouldBeParsedTo` (natT ~> boolT ~> unitT)
     it "(Nat -> Bool) -> Unit" $
-      parseType "(Nat -> Bool) -> Unit" `isParsedTo` (TypeParens (natT ~> boolT) ~> unitT)
+      parseType "(Nat -> Bool) -> Unit" `shouldBeParsedTo` (TypeParens (natT ~> boolT) ~> unitT)
   describe "equalities" $ do
-    it "(a) ~ a"   $ parseType "(Nat)"   `isParsedTo` natT
-    it "((a)) ~ a" $ parseType "((Nat))" `isParsedTo` natT
+    it "(a) ~ a"   $ parseType "(Nat)"   `shouldBeParsedTo` natT
+    it "((a)) ~ a" $ parseType "((Nat))" `shouldBeParsedTo` natT
     it "a -> (b) ~ a -> b" $ do
-      parseType "Nat -> (Bool -> Unit)" `isParsedTo` (natT ~> boolT ~> unitT)
-      parseType "(Nat -> Unit) -> (Unit -> Bool)" `isParsedTo` (TypeParens (natT ~> unitT) ~> unitT ~> boolT)
+      parseType "Nat -> (Bool -> Unit)" `shouldBeParsedTo` (natT ~> boolT ~> unitT)
+      parseType "(Nat -> Unit) -> (Unit -> Bool)" `shouldBeParsedTo` (TypeParens (natT ~> unitT) ~> unitT ~> boolT)
 
 
-spec_lambda_abstractions_can_be_parsed_correctly :: Spec
-spec_lambda_abstractions_can_be_parsed_correctly = do
-  it "basic forms" $ do
-    parseExpr "\\n:Nat.n"  `isParsedTo` trivial "n" natT
-    parseExpr "\\x:Bool.x" `isParsedTo` trivial "x" boolT
-    parseExpr "\\x:Unit.x" `isParsedTo` trivial "x" unitT
-  it "programmatic codes" $
-    parseExpr programmaticCode `isParsedTo` programmaticExpr
+spec_lambda_forms_can_be_parsed_correctly :: Spec
+spec_lambda_forms_can_be_parsed_correctly = do
+  it "abstractions" $ do
+    parseExpr "\\n:Nat.n"  `shouldBeParsedTo` trivial "n" natT
+    parseExpr "\\x:Bool.x" `shouldBeParsedTo` trivial "x" boolT
+    parseExpr "\\x:Unit.x" `shouldBeParsedTo` trivial "x" unitT
+  it "real world codes" $
+    parseExpr code `shouldBeParsedTo` exprOfCode
   where
     trivial :: Identifier -> Type -> Expr
-    trivial x t = ExprLambda $ LambdaAbst x t (ExprLambda $ LambdaIdent x)
+    trivial x t = lambda x t $ ident x
 
-    programmaticCode =
+    churchNum :: String -> String -> Int -> Expr
+    churchNum s z n =
+      let num = replicate n $ LambdaIdent s
+      in lambda s (unitT ~> unitT) . lambda z unitT $
+          foldr ((ExprParens.) . apply) (ident z) num
+
+    code =
       "\\plus:Nat -> Nat -> Nat. \\n : Nat. \\m : Nat.\n" <>
       "   if equal m 0\n" <>
       "     then n\n" <>
       "     else plus (succ n) (pred m)"
 
-    programmaticExpr =
+    exprOfCode =
       lambda "plus" (natT ~> natT ~> natT) . lambda "n" natT . lambda "m" natT $
         if_ condClause
           thenClause
